78e6d1b5a6a191b040f45578d79a78f66a09039a
miaomiao123d <hyperionscyllaastraeavalkyrie@gmail.com>
2025-04-09 09:36:15 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Configure the base path if you're deploying to a GitHub Pages project page
-  // For a user/organization site, you can remove this line
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
   images: {
     unoptimized: true,
   },
